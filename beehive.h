23471227aba424419d14f3146fe7a87ed7cb8874
tutorcs <tutorcs@163.com>
2023-01-26 17:32:35 +0800

    WeChat cstutorcs game of life assignment

---
+++ beehive.h
@@ -0,0 +1,20 @@
+https://tutorcs.com
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+/* File: beehive.h */
+
+//   ## 
+//  #  #
+//   ## 
+
+#include "stdint.h"
+
+#define BEEHIVE_HEIGHT (3)
+#define BEEHIVE_WIDTH (4)
+
+uint8_t beehive[BEEHIVE_HEIGHT][BEEHIVE_WIDTH] = {
+    {0, 1, 1, 0},
+    {1, 0, 0, 1},
+    {0, 1, 1, 0},
+};
---
+++ glider.h
@@ -0,0 +1,20 @@
+https://tutorcs.com
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+/* File: glider.h */
+
+//   # 
+//    #
+//  ###
+
+#include "stdint.h"
+
+#define GLIDER_HEIGHT (3)
+#define GLIDER_WIDTH (3)
+
+uint8_t glider[GLIDER_HEIGHT][GLIDER_WIDTH] = {
+    {0, 1, 0},
+    {0, 0, 1},
+    {1, 1, 1},
+};
